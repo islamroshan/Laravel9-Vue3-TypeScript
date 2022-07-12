ac8cd65bd4a4271c85b1621b3c317652a8c83dd5
islamroshan <islamroshan103@gmail.com>
2022-07-13 04:14:54 +0500

    Laravel 9 + Vue 3

---
+++ resources/js/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+   <center> <h2>Development Starter Project</h2> <h1>Laravel 9 + Vue 3 + Vue Class Component + Vuex + TypeScript</h1></center>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {
+   
+  },
+})
+export default class App extends Vue {
+  initial = 0
+
+  increment() {
+    this.initial += 1
+  }
+  mounted() {
+    console.log('hello');
+  }
+}
+</script>
+<style scoped>
+.progress-bar {
+  height: 0.2rem;
+}
+</style>
---
+++ resources/js/app.ts
@@ -0,0 +1,79 @@
+import './bootstrap.js'
+import { createApp } from 'vue';
+import App from "./App.vue";
+// import router from './router/router';
+// import store from './store/store';
+
+import PrimeVue from "primevue/config";
+import Menubar from 'primevue/menubar';
+import Sidebar from 'primevue/sidebar';
+import Button from 'primevue/button';
+import Image from 'primevue/image';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Card from 'primevue/card';
+import Dropdown from 'primevue/dropdown';
+import Textarea from 'primevue/textarea';
+import Calendar from 'primevue/calendar';
+import ProgressBar from 'primevue/progressbar';
+import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';
+import FileUpload from 'primevue/fileupload';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Dialog from 'primevue/dialog';
+import ConfirmationService from 'primevue/confirmationservice';
+import ConfirmDialog from 'primevue/confirmdialog';
+import Toolbar from 'primevue/toolbar';
+import ProgressSpinner from 'primevue/progressspinner';
+import MultiSelect from 'primevue/multiselect';
+import RadioButton from 'primevue/radiobutton';
+import YouTube from 'vue3-youtube';
+import Avatar from 'primevue/avatar';
+import DataView from 'primevue/dataview';
+import Editor from 'primevue/editor';
+import InputNumber from 'primevue/inputnumber';
+import Tooltip from 'primevue/tooltip';
+
+import 'primevue/resources/themes/lara-light-blue/theme.css';
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import 'primeflex/primeflex.css';
+
+const app = createApp(App);
+app.use(PrimeVue, { ripple: true, inputStyle: 'outlined' });
+// app.use(router);
+// app.use(store);
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.component('YouTube', YouTube)
+app.component('Menubar', Menubar);
+app.component('Sidebar', Sidebar);
+app.component('Button', Button);
+app.component('Image', Image);
+app.component('InputText', InputText);
+app.component('Password', Password);
+app.component('Dropdown', Dropdown);
+app.component('Textarea', Textarea);
+app.component('Card', Card);
+app.component('Calendar', Calendar);
+app.component('ProgressBar', ProgressBar);
+app.component('Toast', Toast);
+app.component('FileUpload', FileUpload);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('Dialog', Dialog);
+app.component('ConfirmDialog', ConfirmDialog);
+app.component('Toolbar', Toolbar);
+app.component('ProgressSpinner', ProgressSpinner);
+app.component('MultiSelect', MultiSelect);
+app.component('RadioButton', RadioButton);
+app.component('Avatar', Avatar);
+app.component('DataView', DataView);
+app.component('Editor', Editor);
+app.component('InputNumber', InputNumber);
+
+app.directive('tooltip', Tooltip);
+
+app.mount('#app');
